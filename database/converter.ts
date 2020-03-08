e6f95e49c1ec561dee3df63e73c1b79b91ed0bda
Hiroyuki Imura <sinewave.440@gmail.com>
2020-03-08 22:58:05 +0900

    ランダムな路線を返すAPIを追加

--- database/converter.ts
+++ database/converter.ts
@@ -1,23 +1,41 @@
 import * as csvtojson from 'csvtojson';
 import * as fs from "fs";
+import * as _ from 'lodash';
 
 (async () => {
     let stations, lines, companies, joins, output = {};
 
     stations = await csvtojson().fromFile("./csv/station20200306free.csv");
+    const station_count = stations.length;
+
     lines = await csvtojson().fromFile("./csv/line20200306free.csv");
+    const line_count = lines.length;
+
     companies = await csvtojson().fromFile("./csv/company20200306.csv");
+    const company_count = companies.length;
+
     joins = await csvtojson().fromFile("./csv/join20200306.csv");
 
-    stations = stations.reduce((acc, station) => {
+    let station_random_indexes = _.range(station_count);
+    station_random_indexes = _.shuffle(station_random_indexes);
+    stations = stations.reduce((acc, station, index) => {
+        station.index = station_random_indexes[index];
         acc[station.station_cd] = station;
         return acc;
     }, {});
-    lines = lines.reduce((acc, line) => {
+
+    let line_random_indexes = _.range(line_count);
+    line_random_indexes = _.shuffle(line_random_indexes);
+    lines = _.reduce(lines, (acc, line, index) => {
+        line.index = line_random_indexes[index];
         acc[line.line_cd] = line;
         return acc;
     }, {});
-    companies = companies.reduce((acc, company) => {
+
+    let company_random_indexes = _.range(company_count);
+    company_random_indexes = _.shuffle(company_random_indexes);
+    companies = companies.reduce((acc, company, index) => {
+        company.index = company_random_indexes[index];
         acc[company.company_cd] = company;
         return acc;
     }, {});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+// TODO: データから全件数を取得するようにする。
+const LINE_COUNT = 617;
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -24,12 +27,11 @@ export const stations = functions.https.onRequest(async (req, res) => {
         if (data) {
             res.status(200).send(data);
         } else {
-            res.status(404).send({ "msg": "Not Found" });
+            res.status(404).send({ msg: "Not Found" });
         }
     } catch (err) {
         res.status(500).send(err);
     }
-
 });
 
 export const lines = functions.https.onRequest(async (req, res) => {
@@ -49,6 +51,22 @@ export const companies = functions.https.onRequest(async (req, res) => {
 export const joins = functions.https.onRequest(async (req, res) => {
     const query = (req.params["0"] === "") ? "joins" : "joins" + req.params["0"];
     const ref = db.ref(query);
+    const data = await ref.once("value");
+    res.send(data);
+});
+
+export const randomLines = functions.https.onRequest(async (req, res) => {
+    if (!req.query.count) {
+        res.status(400).send({msg: "Count parameter is miss."});
+        return;
+    }
+
+    const index = Math.floor(Math.random() * LINE_COUNT + 1);
+    const ref = db.ref("lines")
+        .orderByChild("index")
+        .startAt(index)
+        .limitToFirst(Number(req.query.count));
+
     const data = await ref.once("value");
     res.send(data);
 });
